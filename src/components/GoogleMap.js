b34765ad9aa60e893b00d6a0cf370fed044a13a3
Giovanni Alluvatti <giovanni.alluvatti@lastminute.com>
2018-09-28 08:11:36 +0200

    initialize map and put markers

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,35 +1,86 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import GoogleMapReact from 'google-map-react';
+import React, {Component} from 'react';
 
-const Wrapper = styled.main`
-  width: 100%;
-  height: 100%;
-`;
+class GoogleMap extends Component {
 
-const GoogleMap = ({ children, ...props }) => (
-    <Wrapper>
-        <GoogleMapReact
-            bootstrapURLKeys={{
-                key: "AIzaSyCGekEiZ6lE1XN9rQrmIAqbnN-pF7xMX60"
-            }}
-            {...props}
-        >
-            {children}
-        </GoogleMapReact>
-    </Wrapper>
-);
+    constructor(props) {
+        super(props);
+        this.state = {
+            pointsOfInterest: [
+                {
+                    title: "Duomo",
+                    lat: 45.4640976,
+                    lng: 9.1919265
+                },
+                {
+                    title: "Teatro della Scala",
+                    lat: 45.4663866,
+                    lng: 9.1889054
+                },
+                {
+                    title: "Ristorante Cracco",
+                    lat: 45.4653468,
+                    lng: 9.189871
+                },
+                {
+                    title: "Piazza dei Mercanti",
+                    lat: 45.4645779,
+                    lng: 9.1877259
+                }, {
+                    title: "Museo del 900",
+                    lat: 45.4634873,
+                    lng: 9.1902915
+                },
+                {
+                    title: "Palazzo Reale",
+                    lat: 45.4631162,
+                    lng: 9.191188
+                }
+            ],
+            markers: []
+        }
+        this.loadMap = this.loadMap.bind(this);
+    }
 
-GoogleMap.propTypes = {
-    children: PropTypes.oneOfType([
-        PropTypes.node,
-        PropTypes.arrayOf(PropTypes.node),
-    ]),
-};
+    loadMap() {
+        console.log("Init Map")
+        var mapView = document.getElementById('map');
+        mapView.style.height = window.innerHeight + "px";
+        mapView.style.width = window.innerWidth + "px";
+        const map = new window.google.maps.Map(mapView, {
+            zoom: 16,
+            center: {lat: 45.4640976, lng: 9.1919265}
+        });
+        this.setState({map: map});
+        const markers = [];
+        this.state.pointsOfInterest.forEach((point) => {
+            const marker = new window.google.maps.Marker({
+                position: {lat: point.lat, lng: point.lng},
+                map: map,
+                title: point.title
+            });
 
-GoogleMap.defaultProps = {
-    children: null,
-};
+            markers.push(marker);
+        })
+        this.setState({markers: markers});
+    }
+
+    componentDidMount() {
+        //Loading Google Maps API
+        window.loadMap = this.loadMap;
+        var script = document.createElement("script");
+        script.src = "https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyCGekEiZ6lE1XN9rQrmIAqbnN-pF7xMX60&callback=loadMap";
+        script.defer = true;
+        script.async = true;
+        document.body.appendChild(script);
+        console.log('Script Loaded')
+    }
+
+    render() {
+        console.log('rendering component')
+        return (
+            <div id='map'></div>
+        )
+    }
+}
 
 export default GoogleMap;
